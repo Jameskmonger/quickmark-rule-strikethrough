53ea14cd734ea819a8e4e541c91e0ac2623c7d23
James Monger <jameskmonger@hotmail.co.uk>
2016-04-21 23:31:10 +0100

    Test for undefined for plain text

--- test.js
+++ test.js
@@ -2,3 +2,15 @@
 
 const test = require('modunit');
 const strikethrough = require('./index');
+
+test('it returns undefined for plain text', (t, input) => {
+
+    let output = strong(input);
+
+    t.assert.equal(undefined, output);
+
+}, [
+    ['apples and oranges'],
+    ['plain text here'],
+    ['woop dee doo']
+]);
